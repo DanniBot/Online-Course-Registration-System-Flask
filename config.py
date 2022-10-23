63af0450583d6cca929fa7b28b3cd184a991a696
KiraCodingBot <helloworld.stephanie@gmail.com>
2022-10-23 18:47:17 -0400

    HIDE CONFIG

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or b'}\xecUG\x9f\xf9\x18\xfaVQ$k\xf3RJ\xe4'
 
     MONGODB_SETTINGS = {
         'db': 'OCRSdb',
-        'host': 'mongodb+srv://kira:Ilovesh221b.@ocrsflask.o9arh10.mongodb.net/OCRSdb?retryWrites=true&w=majority'
+        'host': os.environ.get('MONGODB_URI')
     }
